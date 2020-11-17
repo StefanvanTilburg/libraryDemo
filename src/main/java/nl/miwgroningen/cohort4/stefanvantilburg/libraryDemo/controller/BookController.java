1818a5c2101eada3a815bebae190132322c4377e
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-11-17 12:53:25 +0100

    Added bookForm and Postmethod logic

--- src/main/java/nl/miwgroningen/cohort4/stefanvantilburg/libraryDemo/controller/BookController.java
+++ src/main/java/nl/miwgroningen/cohort4/stefanvantilburg/libraryDemo/controller/BookController.java
@@ -1,10 +1,14 @@
 package nl.miwgroningen.cohort4.stefanvantilburg.libraryDemo.controller;
 
+import nl.miwgroningen.cohort4.stefanvantilburg.libraryDemo.model.Book;
 import nl.miwgroningen.cohort4.stefanvantilburg.libraryDemo.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author Stefan van Tilburg <s.g.van.tilburg@st.hanze.nl>
@@ -22,8 +26,26 @@ public class BookController {
     // moet gebruikt worden met protected keyword
     // returned timeleaf html bestand naam
     @GetMapping({"/", "/books"})
-    protected String showBooks(Model model){
+    protected String showBooks(Model model) {
         model.addAttribute("allBooks", bookRepository.findAll());
         return "bookOverview";
+    }
+
+    @GetMapping("/books/add")
+    protected String showBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "bookForm";
+    }
+
+    // Dit noem je binding : @ModelAttribute("book") Book book
+    // Dat ga je controleren in je BindingResult
+    @PostMapping("/books/add")
+    protected String saveOrUpdateBook(@ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "bookForm";
+        } else {
+            bookRepository.save(book);
+            return "redirect:/books";
+        }
     }
 }
