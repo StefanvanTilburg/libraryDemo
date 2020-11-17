cbe94e49395a4b594d310ffc89585c7d64a4213d
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-11-17 10:14:44 +0100

    Added first Book Class

---
+++ src/main/java/nl/miwgroningen/cohort4/stefanvantilburg/libraryDemo/model/Book.java
@@ -0,0 +1,20 @@
+package nl.miwgroningen.cohort4.stefanvantilburg.libraryDemo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Stefan van Tilburg
+ *
+ *
+ */
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer bookID;
+
+    private String title;
+}
