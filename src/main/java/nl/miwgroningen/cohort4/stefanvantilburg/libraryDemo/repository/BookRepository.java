6018fca4d55dc8a47a0693067012d9c7465a4d33
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-11-17 10:18:41 +0100

    Added Book Interface for Database

---
+++ src/main/java/nl/miwgroningen/cohort4/stefanvantilburg/libraryDemo/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package nl.miwgroningen.cohort4.stefanvantilburg.libraryDemo.repository;
+
+import nl.miwgroningen.cohort4.stefanvantilburg.libraryDemo.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author Stefan van Tilburg <s.g.van.tilburg@st.hanze.nl>
+ *
+ * Haal boeken uit en schrijf boeken naar de database
+ */
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
